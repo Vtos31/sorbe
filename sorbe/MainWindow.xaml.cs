3944a0f026bf1580fa0e554b2fc22b151c9bb02e
Vtos31 <zomyban@gmail.com>
2024-12-15 16:05:53 +0200

    Initial commit with .gitignore

--- sorbe/MainWindow.xaml.cs
+++ sorbe/MainWindow.xaml.cs
@@ -17,39 +17,35 @@ namespace sorbe
         public MainWindow()
         {
             InitializeComponent();
-            for(int g =  0; g < 10; g++)
-            {
-                Grid Topicgroup = new Grid() { };
+            MainPage mainPage = new MainPage();
+            Main.Content = mainPage;
+        }
+        public void EnterToInfoPage_Click(object sender, RoutedEventArgs e)
+        {
+            InfoPage InfoPage = new InfoPage();
+            Main.Content = InfoPage;
+        }
 
-                Label TopicLabel = new Label() { Content = "\"CONTEXT\"", HorizontalAlignment = HorizontalAlignment.Left, Margin = new Thickness(20, 10, 0, 0), VerticalAlignment = VerticalAlignment.Top, Height = 68, Width = 534, Foreground = Brushes.White, FontFamily = new FontFamily("Yu Gothic Medium"), FontSize = 44 };
-                Topicgroup.Children.Add(TopicLabel);
+        private void SignIn_Click(object sender, RoutedEventArgs e)
+        {
+            AcountEnterPanel.Visibility = Visibility.Visible;
+            AcountEnterTitle.Content = "Реєстрація";
+            NonLogInPole.Visibility = Visibility.Visible;
+            NonLogInText.Visibility = Visibility.Visible;
+        }
 
-                ScrollViewer ScrollList = new ScrollViewer() { Height = 256, VerticalScrollBarVisibility = ScrollBarVisibility.Disabled, HorizontalScrollBarVisibility = ScrollBarVisibility.Visible };
-                Topicgroup.Children.Add(ScrollList);
-                StackPanel List = new StackPanel() { Margin = new Thickness(32, 112, 58, 49), Orientation = Orientation.Horizontal };
-                ScrollList.Content = List;
+        private void AcountEnterPanelExit_Click(object sender, RoutedEventArgs e)
+        {
+            AcountEnterPanel.Visibility = Visibility.Collapsed;
+        }
 
-
-                for (int i = 0; i < 20; i++)
-                {
-                    Button button = new Button
-                    {
-                        Width = 200,
-                        Height = 200,
-                        Margin = new Thickness(10),
-                        Background = new ImageBrush
-                        {
-                            ImageSource = new BitmapImage(new Uri("/PRAY FOR PARIS WALLPAPER 1920x1080 - Imgur.png")),
-                            Stretch = Stretch.UniformToFill
-                        }
-                    };
-                    List.Children.Add(button);
-                }
-
-                GridTestMOW.Children.Add(Topicgroup);
-            }
-           
-
+        private void LogIn_Click(object sender, RoutedEventArgs e)
+        {
+            AcountEnterPanel.Visibility = Visibility.Visible;
+            AcountEnterTitle.Content = "Авторизація";
+            NonLogInPole.Visibility = Visibility.Collapsed;
+            NonLogInText.Visibility = Visibility.Collapsed;
         }
     }
+
 }
